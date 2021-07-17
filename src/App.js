5446cff1ebae557363c7bdbe0e310ed08f281356
riad <skmdriad@gmail.com>
2021-07-17 19:30:01 +0800

    update

--- src/App.js
+++ src/App.js
@@ -1,21 +1,12 @@
 
 import './App.css';
-import { Button,Card } from 'react-bootstrap';
+import Home from './Components/Home/Home';
 
 function App() {
   return (
-    <div className="App">
-      <Card>
-        <Card.Header>Featured</Card.Header>
-        <Card.Body>
-          <Card.Title>Special title treatment</Card.Title>
-            <Card.Text>
-              With supporting text below as a natural lead-in to additional content.
-            </Card.Text>
-            <Button variant="primary">Go somewhere</Button>
-        </Card.Body>
-      </Card>
-    </div>
+    <>
+      <Home></Home>
+    </>
   );
 }
 
