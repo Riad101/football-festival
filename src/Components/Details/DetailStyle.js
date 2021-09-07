18ad511e0b31bfdb6e71b6dbf6cb94315ec4f461
riad <skmdriad@gmail.com>
2021-09-07 23:48:15 +0800

    added styles

--- src/Components/Details/DetailStyle.js
+++ src/Components/Details/DetailStyle.js
@@ -4,6 +4,12 @@ const useStyles = makeStyles((theme)=>({
   container:{
     background: theme.palette.background.paper,
     padding: theme.spacing(8, 0, 6)
-  }
+  },
+  cardMedia : {
+    paddingTop: '56.25%'
+},
+cardContent : {
+  flexGrow: 1,
+},
   }));
   export default useStyles;
